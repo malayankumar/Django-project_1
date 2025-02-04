4f9cc171e6830efe27b04c7255562666b7d296cc
malayankumar <malayankumar@gmail.com>
2025-02-04 15:55:14 +0530

    add home page and product model migration; update URLs and views

--- shop419/mainapp/views.py
+++ shop419/mainapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def homeView(request):
+    products = Product.objects.all()
+    context ={
+        'product_list': products
+    }
+    return 
