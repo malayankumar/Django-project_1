82df38c2c6b2b85ae40d761115f6850efc723e1b
malayankumar <malayankumar@gmail.com>
2025-02-04 14:21:36 +0530

    product admin changes

--- shop419/mainapp/models.py
+++ shop419/mainapp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.PositiveIntergerField(null = False)
+    price = models.PositiveIntegerField(null = False)
     desc = models.TextField()
     pic = models.ImageField(upload_to = "products/", null = False)
     stock = models.PositiveIntegerField(default = 1)
