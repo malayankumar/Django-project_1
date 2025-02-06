f0afaa3af3a23a28f38848b4c2e34b9c9b2846d2
malayankumar <malayankumar@gmail.com>
2025-02-06 14:18:35 +0530

    view changes

--- shop419/mainapp/views.py
+++ shop419/mainapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Product
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 def homeView(request):
     products = Product.objects.all()
     context ={
         'product_list': products
     }
-    return 
+    template = loader.get_template('mainapp/home.html')
+    return HttpResponse(templates.render(context, request))
