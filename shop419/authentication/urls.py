6ccb18d86638a0b6e7e33bb4b051f5afa0a01564
malayankumar <malayankumar@gmail.com>
2025-02-13 14:36:02 +0530

    auth view changes

--- shop419/authentication/urls.py
+++ shop419/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 
 urlpatterns = [
-    path("", views.loginview, name = "login"),
+    path("login", views.LoginView, name = "login"),
 ]
--- shop419/authentication/views.py
+++ shop419/authentication/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render,redirect
 from django.url import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
 
-def UserRegistration(createview):
+class UserRegistration(createview):
     form_class = UserCreationForm
     template_name = 'register.html'
-    success_url = reverse_lazy('login')
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
-
+    success_url = reverse_lazy('signin')
 # Create your views here.
+class Login(LoginView):
+    template_name = 'login.html'
+
