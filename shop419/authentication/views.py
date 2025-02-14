7c8f44a2e2223127d43f0b33b032e696cc995e72
malayankumar <malayankumar@gmail.com>
2025-02-14 14:07:17 +0530

    sign in changes

--- shop419/authentication/views.py
+++ shop419/authentication/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView
 
 class UserRegistration(CreateView):
--- shop419/shop419/settings.py
+++ shop419/shop419/settings.py
@@ -128,3 +128,7 @@ MEDIA_URL = '/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'signin'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
