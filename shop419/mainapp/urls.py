fd7ed02ccf2f765332cc7a1ccb01112fe084f870
malayankumar <malayankumar@gmail.com>
2025-02-17 15:54:47 +0530

    adding products html

--- shop419/mainapp/urls.py
+++ shop419/mainapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path("", views.homeView, name = "home"),
     path("about", views.aboutView, name = "aboutpage"),
-    path("contacts", views.contactsView, name = "contactspage"),
+    path("products/add", views.AddProduct.as_view(), name = "addproduct"),
+    path("Products/int<pk>", views.ProductDetails.as_view(), name = "prod_details"),
+    path("Products/edit/<int:pk>", views.UpdateProduct.as_view(), name = "editproduct"),
+    path("Products/del/<int:pk>", views.DeleteProduct.as_view(), name = "delproduct"),
 ]
--- shop419/mainapp/views.py
+++ shop419/mainapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Product
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+from django.urls import reverse_lazy, reverse
 # Create your views here.
 def homeView(request):
     products = Product.objects.all()
@@ -25,9 +27,30 @@ def aboutView(request):
     template = loader.get_template('about.html')
     return HttpResponse(template.render(context, request))
 
-def contactsView(request):
-    context = {
+class AddProduct(CreateView):
+    model = Product
+    fields = ['name', 'price', 'desc', 'pic', 'stock']
+    template_name = 'addproduct.html'
+    success_url = reverse_lazy('home')
 
-    }
-    template = loader.get_template('contacts.html')
-    return HttpResponse(template.render(context, request))
+#read
+class ProductDetails(DetailView):
+    model = Product
+    template_name = 'prod_details.html'
+    context_object_name = 'product'
+
+#update
+class UpdateProduct(UpdateView):
+    model = Product
+    fields = '__all__'
+    template_name = 'editproduct.html'
+    def get_success_url(self):
+        return reverse_lazy('prod_details', kwargs={'pk': self.object.pk})
+
+
+#delete
+class DeleteProduct(DeleteView):
+    model = Product
+    template_name = 'delproduct.html'
+    success_url = reverse_lazy('home')
+
