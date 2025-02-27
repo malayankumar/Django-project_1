95976b33f0f1318f0e81231ddf2995a610365f22
malayankumar <malayankumar@gmail.com>
2025-02-27 16:16:44 +0530

    cart js

--- shop419/cart/views.py
+++ shop419/cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from mainapp.models import Product
 from .models import CartItem
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -33,3 +35,30 @@ def viewCart(request):
         'total_price' : total_price
     }
     return render( request, template, context)
+
+def remFromCart(request, cart_item_id):
+    this_cart_item = CartItem.objects.get(id = cart_item_id)
+    this_cart_item.delete()
+    return redirect('view_cart')
+
+
+@login_required
+def addQuantity(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id, user=request.user)
+    cart_item.quantity += 1
+    cart_item.save()
+    overall_total = sum(item.get_total_price() for item in CartItem.objects.filter(user=request.user))
+    return JsonResponse({'quantity': cart_item.quantity, 'total_price': cart_item.get_total_price(), 'overall_total': overall_total})
+
+@login_required
+def remQuantity(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id, user=request.user)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+        overall_total = sum(item.get_total_price() for item in CartItem.objects.filter(user=request.user))
+        return JsonResponse({'quantity': cart_item.quantity, 'total_price': cart_item.get_total_price(), 'overall_total': overall_total})
+    else:
+        cart_item.delete()
+        overall_total = sum(item.get_total_price() for item in CartItem.objects.filter(user=request.user))
+        return JsonResponse({'quantity': 0, 'total_price': 0, 'overall_total': overall_total})
